69301ddfae66d92d8e19f194c4dff2ef866f0b17
Vonamosip <sashadrozdov94@gmail.com>
2025-06-09 16:34:35 +0300

    create project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { RegisterComponent } from './pages/auth/components/register/register.component';
+import { LoginComponent } from './pages/auth/components/login/login.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/store/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadComponent: () => AuthComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
---
+++ src/app/pages/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.scss'
+})
+export class AuthComponent {
+
+}
